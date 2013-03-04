02c65a575aa87eacc74269c3ba4100f073a3420a
Hargobind S. Khalsa <khalsah@gmail.com>
2013-03-03 21:04:04 -0700

    Add indexing to SolutionEngine

--- src/com/github/noctorii/scrabblesolver/SolutionEngine.java
+++ src/com/github/noctorii/scrabblesolver/SolutionEngine.java
@@ -1,5 +1,7 @@
 package com.github.noctorii.scrabblesolver;
 
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.Set;
 
 public class SolutionEngine {
@@ -7,12 +9,26 @@ public class SolutionEngine {
     private Set<String> words;
     private Set<String> subwords;
 
-    public SolutionEngine (Iterable<String> words, ScoringEngine scoringEngine){
+    public SolutionEngine (Collection<String> words, ScoringEngine scoringEngine){
         this.scoringEngine = scoringEngine;
+      
+        this.words = new HashSet<String>(words);
+        this.subwords = new HashSet<String>();
+        for(String word : words) subindex(word);
+    }
+
+    private void subindex(String word) {
+        index(word.substring(0, word.length()-1));
+        index(word.substring(1, word.length()));
+    }
+
+    private void index(String word) {
+        if(word.isEmpty() || subwords.contains(word)) return;
+        subwords.add(word);
+        subindex(word);
     }
 
     public Play solve (GameState state, Hand hand){
-
+        return null;
     }
-
 }
